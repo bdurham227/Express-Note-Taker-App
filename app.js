3340d348423d5daed39f0da59330f39eba6f0319
bdurham227 <bdurham227@gmail.com>
2021-04-20 16:49:53 -0400

    adding get requests

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const path = require('path');
+const { RSA_NO_PADDING } = require('constants');
 
-//middleware
+//-----1) Middleware---------------------
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+
+//read and parse db.json 
+const notes = JSON.parse(fs.readFileSync(`${__dirname}/db/db.json`));
+
+
+
+
+//-----2) Routes-----
+//get all notes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/v1/notes', (req, res) => {
+    res.sendFile(paht.join(__dirname, 'db/db.json'));
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+
+
+
+
 
 
 
