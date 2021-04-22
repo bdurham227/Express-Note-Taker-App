a1d947a4722dec3466b72d63675994c28b28cc05
bdurham227 <bdurham227@gmail.com>
2021-04-22 10:37:11 -0400

    got delete working. pushing this up before i work on refactoring

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,73 +1,116 @@
 const fs = require('fs');
+const path = require('path');
 const uuid = require('uuid');
 
 
 const notes = JSON.parse(fs.readFileSync(`${__dirname}/../db/db.json`));
 
 module.exports = (app) => {
-    app.get('/api/notes', (req, res) => res.json(notes));
-
-
-    // app.get('/api/notes/:id', (req, res) => {
-    //     console.log(req.params);
-    //     const id = req.params.id;
-    //     const note = notes.filter(el => el.id === id);
-
-    //     if (!note) {
-    //         return res.status(404).json({
-    //             status: 'fail',
-    //             message: 'Invalid ID'
-    //         })
-    //     }
+    // app.get('/api/notes', (req, res) => {
     //     res.status(200).json({
     //         status: 'success',
+    //         results: notes.length,
     //         data: {
-    //             note
+    //             notes
     //         }
     //     })
     // });
+    app.get('/api/notes', (req, res) => res.json(notes))
 
 
+    // app.get('/api/notes/:id', (req, res) => {
+
+    // })
+
+
+
+    // app.post('/api/notes', (req, res) => {
+    //     // const newTitle = notes[notes.length -1 ].title + 1;
+    //     // const newId = uuid.v4();
+    //     try {
+    //         // req.body.id = notes.length;
+    //         // const notesId = req.body.id;
+    //         const newNote = Object.assign({ id: uuid.v4(), title: req.body.title, text: req.body.text });
+    //         notes.push(newNote);
+    //         fs.writeFile(`${__dirname}/../db/db.json`, JSON.stringify(notes), err => {
+    //             res.status(200).json({
+    //                 status: 'success',
+    //                 data: {
+    //                     notes: newNote
+    //                 }
+    //             });
+    //         })
+    //     } catch(err){
+    //         console.log(err)
+    //     }
+       
+    // })
 
     app.post('/api/notes', (req, res) => {
-        // const newTitle = notes[notes.length -1 ].title + 1;
-        // const newId = uuid.v4();
-        try {
-            const newNote = Object.assign({ id: uuid.v4(), title: req.body.title, text: req.body.text });
-            notes.push(newNote);
-            fs.writeFile(`${__dirname}/db/db.json`, JSON.stringify(notes), err => {
-                res.status(200).json({
-                    status: 'success',
-                    data: {
-                        notes: newNote
-                    }
-                });
+        const  noteId  = notes.length;
+        const newTitle = req.body.title;
+        const newNote = Object.assign({ id: noteId, title: newTitle, text: req.body.text });
+        notes.push(newNote);
+        fs.writeFile(`${__dirname}/../db/db.json`, JSON.stringify(notes), err => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    notes: newNote
+                }
             })
-        } catch(err){
-            console.log('error!')
+        })
+    })
+    app.delete('/api/notes/:id', (req, res) => {
+        fs.readFileSync('db/db.json');
+        const deletedNote = notes.find((note) => note.id === Number(req.params.id));
+
+        if (!deletedNote) {
+            return res.status(400).json({ 
+                status: 'fail',
+                message: `could not find find that ${deletedNote}`
+            })
         }
-       
+        const newNoteDb = notes.filter((note) => note.id !== Number(req.params.id));
+        fs.writeFile('db/db.json', JSON.stringify(newNoteDb), err => {
+            if (err) throw err;
+        })
+        return res.sendFile(path.join(__dirname, '/../db/db.json'));
     })
 
-  app.delete('/api/notes/:id', (req, res) => {
-      const foundNote = notes.some(note => note.id == req.params.id);
-
-      if (foundNote) {
-        res.status(204).json({
-            status: 'success',
-            message: 'Note Deleted',
-            notes: notes.filter(note => note.id != req.params.id)
-        })
-      } else {
-          res.status(404).json({
-              status: 'fail',
-              message: `No note with the id of ${req.params.id}`
-          });
-      }
-  })
 
 
 
 
 
-}
+//   app.delete('/api/notes/:id', (req, res) => {
+//       try {
+//         fs.readFileSync('db/db.json');
+//     //   const foundNote = notes.some(note => note.id === req.params.id);
+//     const foundNote = req.params.id;
+//       console.log(foundNote);
+
+//         if (foundNote) {
+//             let newNoteArray = notes.filter(note => note.id === req.params.id)
+//             console.log(newNoteArray)
+//             fs.writeFile('db/db.json', JSON.stringify(newNoteArray), err => {
+//                 if (err) throw (err)
+//                 console.log('saved');
+//             });
+//         } else {
+//             res.status(400)
+//         }
+//         return res.sendFile(path.join(__dirname, 'db/db.json'))
+        
+
+//       } catch (err) {
+//           console.error(err);
+//           throw(err)
+//       }
+//   });
+   
+    //    
+
+
+
+
+};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,36 @@
+const { RSA_NO_PADDING } = require('constants');
 const path = require('path');
 
-module.exports = (app) => {
+// module.exports = (app) => {
 
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public/index.html'));
-    })
+//     app.get('/', (req, res) => {
+//         res.sendFile(path.join(__dirname, 'public/index.html'));
+//     })
 
-    app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '/../public/notes.html'));
-    })
+//     app.get('/notes', (req, res) => {
+//         res.sendFile(path.join(__dirname, '/../public/notes.html'));
+//     })
     
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public/index.html'));
-    })
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(__dirname, 'public/index.html'));
+//     })
+// }
+
+module.exports = async (app) => {
+    try {
+        const resFile1 = app.get('/', (req, res) => {
+            res.sendFile(path.join(__dirname, '/../public/index.html'));
+        })
+        const resFile2 = app.get('/notes', (req, res) => {
+            res.sendFile(path.join(__dirname, '/../public/notes.html'));
+        })
+        const resFile3 = app.get('*', (req, res) => {
+            res.sendFile(path.join(__dirname, '/../public/index.html'));
+        }) 
+        const all = await Promise.all([resFile1, resFile2, resFile3]);
+      } catch (err) {
+          console.error(err)
+          throw(err)
+          
+      }
 }
